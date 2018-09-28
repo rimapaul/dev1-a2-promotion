2038e0de6821e140df6afd4fe399eab9a9e2f7d8
Christine Jordan <christine.jordan@shopify.com>
2018-09-28 09:07:55 -0400

    Add promotion.rb

---
+++ promotion.rb
@@ -0,0 +1,17 @@
+class Promotion
+
+  attr_reader :start_date, :end_date
+
+  def initialize(start_date = Date.at(0), end_date = nil)
+    @start_date = start_date
+    @end_date = end_date
+  end
+
+  def discount(order)
+    0
+  end
+
+  def valid?(date)
+    start_date && start_date < date && (!end_date || date < end_date)
+  end
+end
