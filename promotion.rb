d73024265ba7d2228506edce6270a009f6e59e26
Christine Jordan <christine.jordan@shopify.com>
2018-10-22 14:07:51 -0400

    Fix start_date

--- promotion.rb
+++ promotion.rb
@@ -1,8 +1,10 @@
+require 'date'
+
 class Promotion
 
   attr_reader :start_date, :end_date
 
-  def initialize(start_date = Date.at(0), end_date = nil)
+  def initialize(start_date = Date.new(0), end_date = nil)
     @start_date = start_date
     @end_date = end_date
   end
